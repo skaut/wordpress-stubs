0c766bb57da46be05cafd37f813738a7eefcb650
GenaBitu <developer@dedic.eu>
2019-12-02 12:13:38 +0100

    WP_Theme

---
+++ stubs/WordPress/class-wp-theme.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package phan-wordpress-stubs
+ */
+
+/**
+ */
+class WP_Theme implements ArrayAccess {
+	/**
+	 * @var string
+	 */
+	public $version;
+
+	/**
+	 * @param mixed $offset
+	 *
+	 * @return bool
+	 */
+	public function offsetExists( $offset ) {
+	}
+
+	/**
+	 * @param mixed $offset
+	 *
+	 * @return mixed
+	 */
+	public function offsetGet( $offset ) {
+	}
+
+	/**
+	 * @param mixed $offset
+	 * @param mixed $value
+	 *
+	 * @return void
+	 */
+	public function offsetSet( $offset, $value ) {
+	}
+
+	/**
+	 * @param mixed $offset
+	 */
+	public function offsetUnset( $offset ) {
+	}
+}
