708ce2cc36fb3693d387338484086aaa21f6eab8
Marek Dědič <developer@dedic.eu>
2020-03-09 00:16:00 +0100

    WP_HTTP->get()+WP_HTTP->request()+wp_remote_get()

--- stubs/WordPress/functions.php
+++ stubs/WordPress/functions.php
@@ -796,6 +796,43 @@ function wp_register_style( $handle, $src, $deps = array(), $ver = false, $media
 }
 
 /**
+ * @param string       $url
+ * @param string|array $args {
+ *     @type string       $method
+ *     @type int          $timeout
+ *     @type int          $redirection
+ *     @type string       $httpversion
+ *     @type string       $user-agent
+ *     @type bool         $reject_unsafe_urls
+ *     @type bool         $blocking
+ *     @type string|array $headers
+ *     @type array        $cookies
+ *     @type string|array $body
+ *     @type bool         $compress
+ *     @type bool         $decompress
+ *     @type bool         $sslverify
+ *     @type string       $sslcertificates
+ *     @type bool         $stream
+ *     @type string       $filename
+ *     @type int          $limit_response_size
+ * }
+ *
+ * @return WP_Error|array {
+ *     @type Requests_Utility_CaseInsensitiveDictionary $headers
+ *     @type string                                     $body
+ *     @type array                                      $response {
+ *         @type int    $code
+ *         @type string $message
+ *     }
+ *     @type WP_HTTP_Cookie[]          $cookies
+ *     @type string|null               $filename
+ *     @type WP_HTTP_Requests_Response $http_response
+ * }
+ */
+function wp_remote_get( $url, $args = array() ) {
+}
+
+/**
  * @param mixed $response
  * @param int   $status_code
  *
