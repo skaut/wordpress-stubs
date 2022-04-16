08a58d1323faf3849640767891306e09b07e8172
Marek Dědič <developer@dedic.eu>
2022-04-16 11:39:50 +0200

    Added WP_Post_Type

---
+++ stubs/WordPress/class-wp-post-type.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package phan-wordpress-stubs
+ */
+
+/**
+ */
+class WP_Post_Type {
+	/**
+	 * @var string
+	 */
+	public $name;
+
+	/**
+	 * @var string
+	 */
+	public $label;
+
+	/**
+	 * @var stdClass
+	 */
+	public $labels;
+
+	/**
+	 * @var bool
+	 */
+	public $hierarchical;
+}
