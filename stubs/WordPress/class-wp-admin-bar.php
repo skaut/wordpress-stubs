c8c5b130ecf10b5646d40249442d5052104d9d9f
Marek Dědič <developer@dedic.eu>
2022-04-19 14:47:05 +0200

    Full array types for WP_Admin_bar::add_menu()

--- stubs/WordPress/class-wp-admin-bar.php
+++ stubs/WordPress/class-wp-admin-bar.php
@@ -7,7 +7,7 @@
  */
 class WP_Admin_Bar {
 	/**
-	 * @param array $node {
+	 * @param array{id: string, title?: string, parent?: string, href?: string, group?: bool, meta?: array{html?: string, class?: string, rel?: string, onclick?: string, target?: string, title?: string, tabindex?: string}} $node {
 	 *     @type string $id
 	 *     @type string $title
 	 *     @type string $parent
