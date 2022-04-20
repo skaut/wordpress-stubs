e5702ab0a6a5515cee70d293ac4cae5439406322
Marek Dědič <developer@dedic.eu>
2022-04-20 14:58:18 +0200

    Full array types for WP_Customize_Manager::add_control()

--- stubs/WordPress/class-wp-customize-manager.php
+++ stubs/WordPress/class-wp-customize-manager.php
@@ -12,20 +12,24 @@ class WP_Customize_Manager {
 	public $selective_refresh;
 
 	/**
-	 * @param WP_Customize_Control|string $id
-	 * @param array                       $args {
-	 *     @type array    $settings
-	 *     @type string   $setting
-	 *     @type string   $capability
-	 *     @type int      $priority
-	 *     @type string   $section
-	 *     @type string   $label
-	 *     @type string   $description
-	 *     @type array    $choices
-	 *     @type array    $input_attrs
-	 *     @type bool     $allow_addition
-	 *     @type string   $type
-	 *     @type callback $active_callback
+	 * @param WP_Customize_Control|string                                                                                                                                                                                                                                                                                                                                                                 $id
+	 * @param array{instance_number?: int, manager?: WP_Customize_Manager, id?: string, settings?: array<int|string, string>, setting?: string, capability?: string, priority?: int, section?: string, label?: string, description?: string, choices?: array<string, string>, input_attrs?: array<string, string>, allow_addition?: bool, json?: array<mixed>, type?: string, active_callback?: callable} $args {
+	 *     @type int                       $instance_number
+	 *     @type WP_Customize_Manager      $manager
+	 *     @type string                    $id
+	 *     @type array<int|string, string> $settings
+	 *     @type string                    $setting
+	 *     @type string                    $capability
+	 *     @type int                       $priority
+	 *     @type string                    $section
+	 *     @type string                    $label
+	 *     @type string                    $description
+	 *     @type array<string, string>     $choices
+	 *     @type array<string, string>     $input_attrs
+	 *     @type bool                      $allow_addition
+	 *     @type array<mixed>              $json
+	 *     @type string                    $type
+	 *     @type callable                  $active_callback
 	 * }
 	 *
 	 * @return WP_Customize_Control
