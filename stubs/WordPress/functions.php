7c597030b9312b72322eff651c4b34cc6145d9a8
jcvignoli <dev@jcvignoli.com>
2025-01-04 18:01:32 -0300

    fixed comment and removed string declaration

--- stubs/WordPress/functions.php
+++ stubs/WordPress/functions.php
@@ -317,11 +317,11 @@ function checked( $checked, $current = false, $echo = false ) {
 
 /**
  * @param string $capability
- * @param mixed  $args
+ * @param mixed  ...$args
  *
  * @return bool
  */
-function current_user_can( string $capability, ...$args ) {
+function current_user_can( $capability, ...$args ) {
 }
 
 /**
