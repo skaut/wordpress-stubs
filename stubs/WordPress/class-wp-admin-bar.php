5ed24b15ee0a0770a5e2434e3f9a4514cccafcb5
jcvignoli <dev@jcvignoli.com>
2025-01-13 14:31:23 -0300

    properties with null in add_menu() and get_node() methods of WP_Admin_Bar class, improved get_node() that was outdated

--- stubs/WordPress/class-wp-admin-bar.php
+++ stubs/WordPress/class-wp-admin-bar.php
@@ -8,13 +8,13 @@ declare(strict_types = 1);
 class WP_Admin_Bar {
 
 	/**
-	 * @param array{id: string, title?: string, parent?: string|false|null, href?: string, group?: bool, meta?: array{html?: string, class?: string, rel?: string, onclick?: string, target?: string, title?: string, tabindex?: string}} $node {
-	 *     @type string            $id
-	 *     @type string            $title
-	 *     @type string|false|null $parent
-	 *     @type string            $href
-	 *     @type bool              $group
-	 *     @type array             $meta
+	 * @param array{id: string|false|null, title?: string|false|null, parent?: string|false|null, href?: string|false|null, group?: bool, meta?: array{html?: string, class?: string, rel?: string, onclick?: string, target?: string, title?: string|false, tabindex?: string}} $node {
+	 *     @type string|false|null  $id
+	 *     @type string|false|null  $title
+	 *     @type string|false|null  $parent
+	 *     @type string|false|null  $href
+	 *     @type bool               $group
+	 *     @type array              $meta
 	 * }
 	 *
 	 * @return void
@@ -23,10 +23,19 @@ class WP_Admin_Bar {
 	}
 
 	/**
-	 * @param string $id
+	 * @param array{id: string|false|null, title?: string|false|null, parent?: string|false|null, href?: string|false|null, group?: bool, meta?: array{html?: string, class?: string, rel?: string, onclick?: string, target?: string, title?: string|false, tabindex?: string}} $args {
+	 *     Arguments for adding a node.
 	 *
-	 * @return object|null
+	 *     @type string|false|null $id     ID of the item.
+	 *     @type string|false|null $title  Title of the node.
+	 *     @type string|false|null $parent Optional. ID of the parent node.
+	 *     @type string|false|null $href   Optional. Link for the item.
+	 *     @type bool              $group  Optional. Whether or not the node is a group. Default false.
+	 *     @type array             $meta   Meta data including the following keys: 'html', 'class', 'rel', 'lang', 'dir',
+	 *                                  'onclick', 'target', 'title', 'tabindex', 'menu_title'. Default empty.
+	 * }
+	 * @return void
 	 */
-	public function get_node( $id ) {
+	public function get_node( $args ) {
 	}
 }
